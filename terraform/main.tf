3eb5fad41df1f2148809d59ee736f536a1bc6c8a
vimalvijayan <vvimal44@gmail.com>
2023-06-27 20:15:29 +0200

    updated

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+
+  cloud {
+    organization = "optimus"
+
+    workspaces {
+      name = "TerraformCloudOnboarding"
+    }
+  }
+
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.45.0"
+    }
+  }
+}
+
+provider "tfe" {
+  # Configuration options
+}
